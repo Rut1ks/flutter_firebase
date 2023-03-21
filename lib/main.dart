4c64fb4bb2ac56586642d98dc731caf5f66ce68d
Артём Александрович <isip_a.a.podushkin@mpt.ru>
2023-03-21 14:34:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase/login.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(accentColor: Colors.orange, primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
